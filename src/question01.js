b91268e2f414b23e854b9b474a8b4c44f00d258a
PatriquiMarques <patriqui.marques@hotmail.com>
2024-07-16 22:03:38 -0300

    exe

--- src/question01.js
+++ src/question01.js
@@ -1,10 +1,6 @@
-// Escreva um algoritmo que solicite ao usuário um texto ou palavra e ele conte quantas vogais esse texto ou palavra tem.
-//  Vale lembrar que toda String (texto), nada mais é do que um vetor de caracteres.
-
-
 import leia from "readline-sync"
 
-var palavra = leia.question("DIGITE UMA PALAVRA")
+// Escreva um algoritmo que solicite ao usuário um texto ou palavra e ele conte quantas vogais esse texto ou palavra tem. Vale lembrar que toda String (texto), nada mais é do que um vetor de caracteres.
 
 
 
